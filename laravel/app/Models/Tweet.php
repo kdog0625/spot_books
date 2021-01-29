0f3e5c3227d36bdc078888a0d75cf2e3bb81156f
kdog0625 <kdoooog34@gmail.com>
2021-01-29 22:56:30 +0900

    Tweetモデルにfillable追加

--- laravel/app/Models/Tweet.php
+++ laravel/app/Models/Tweet.php
@@ -9,6 +9,11 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Tweet extends Model
 {
+    //不正なリクエストによってtweetsテーブルが予期せぬ内容に更新されることを防ぐように
+    protected $fillable = [
+        'tweet_name',
+        'content',
+    ];
     // use HasFactory;
     public function user(): BelongsTo
     {
